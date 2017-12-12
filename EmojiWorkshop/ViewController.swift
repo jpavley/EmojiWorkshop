bfacbb907687419f114807d05bc2d1458b0c1b3e
John Pavley <jpavley@gmail.com>
2017-12-12 13:06:48 -0500

    Implemented multiple section headers

--- EmojiWorkshop/ViewController.swift
+++ EmojiWorkshop/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         emojiCollection = EmojiCollection(sourceFileName: "emoji-test-5.0")
 
     }
@@ -30,41 +31,51 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let emojiCollection = emojiCollection {
-            return emojiCollection.emojiGlyphs.count
-        }
         
-        return 0
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = emojiGlyphTable.dequeueReusableCell(withIdentifier: "EmojiGlyphCell") as! EmojiGlyphTableViewCell
         
         if let emojiCollection = emojiCollection {
-            let currentEmojiGlyph = emojiCollection.emojiGlyphs[indexPath.row]
+            let currentEmojiGlyph = emojiCollection.emojiGlyphs[(indexPath.section * indexPath.row) + indexPath.row]
+            
             cell.emojiLabel.text = currentEmojiGlyph.glyph
             cell.descriptionLabel.text = currentEmojiGlyph.description
             cell.priorityLabel.text = "Priority \(currentEmojiGlyph.priority)"
+
         }
         
         return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        
         if let emojiCollection = emojiCollection {
             return emojiCollection.sections.count
         }
+        
         return 0
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 45
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let emojiCollection = emojiCollection {
+            return emojiCollection.glyphsIDsInSections[section].count
+        }
+
         return 0
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if let emojiCollection = emojiCollection {
+            return emojiCollection.sections[section]
+        }
+
         return ""
     }
     
