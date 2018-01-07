8d1fd2b87086a24ad7401fcf7233e5772d789a93
John Pavley <jpavley@gmail.com>
2018-01-07 14:23:26 -0500

    added vc for detail view
    renamed VCs for clarity

---
+++ EmojiWorkshop/EmojiDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiDetailViewController.swift
+//  Emoji Spy
+//
+//  Created by John Pavley on 1/7/18.
+//  Copyright © 2018 Epic Loot. All rights reserved.
+//
+
+import UIKit
+
+class EmojiDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
